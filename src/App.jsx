213a1af5f8d31293762dbafcc6bb97511e55f561
rain44556 <bristy44556@gmail.com>
2025-05-01 21:16:06 +0600

    create Weather component with city input and basic weather data rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Weather from './components/Weather/Weather'
 
 function App() {
 
   return (
     <>
      <h1 className='font-bold'>Hello Weather!</h1>
+   <Weather></Weather>
     </>
   )
 }
