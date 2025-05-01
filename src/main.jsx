aa3f2e6043009622adff1d4e843b8e00a80170e3
rain44556 <bristy44556@gmail.com>
2025-05-01 18:04:07 +0600

    added redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
