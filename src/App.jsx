05f9c09c21135e2c82e4fc11e69d587a4adfa6b3
rain44556 <bristy44556@gmail.com>
2025-05-02 20:23:22 +0600

    add custom gradient background colors for theme combiation

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Weather from "./components/Weather/Weather";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-r from-[#F2EFE7] to-[#9ACBD0] dark:bg-gradient-to-r dark:from-[#2C3E50] dark:to-[#34495E] text-black shadow-md dark:text-gray-300">
       <Navbar></Navbar>
       <main>
         <Weather></Weather>
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -31,13 +31,15 @@ const Navbar = () => {
   }, [theme]);
 
   return (
-    <div className=" my-10 ">
-      <div className="flex gap-5 py-3 px-5 items-center max-w-lg mx-auto justify-between mb-5 bg-blue-600 dark:bg-gray-700 dark:text-white">
-        <h1 className="text-2xl sm:text-3xl font-bold">Hello Weather!</h1>
+    <div className="py-10">
+
+      <div className="flex gap-5 py-3 px-5 items-center max-w-lg mx-auto justify-between mb-5">
+        <h1 className="text-2xl sm:text-3xl text-[#006A71] font-bold">Hello Weather!</h1>
         <button 
         onClick={()=> setTheme(theme === "light" ? "dark" : "light")}
-        className="border w-8 h-8 rounded-full cursor-pointer
-        bg-white/10 backdrop-blur-md  border-white/20 hover:bg-white/20 focus:outline-none transition-all duration-300 shadow-lg hover:shadow-xl">
+        className="border w-10 h-10 rounded-full cursor-pointer text-[#006A71] dark:text-gray-100
+        bg-white/10 backdrop-blur-md border-white/20 hover:bg-white/20 focus:outline-none transition-all duration-300 shadow-lg hover:shadow-xl">
+          
           {theme === "light" ? (
             <MdDarkMode className="w-5 h-5 mx-auto"/>
           ) : (
@@ -51,14 +53,14 @@ const Navbar = () => {
           onChange={searchCityName}
           type="search"
           id="search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search City"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gradient-to-r from-[#9ACBD0] to-[#F2EFE7] dark:bg-gray-600 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-300 shadow-md"
+          placeholder="Search City...."
           required
         />
         <button
           onClick={handleSearch}
           type="submit"
-          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="cursor-pointer text-[#F2EFE7] absolute end-2.5 bottom-2.5 bg-[#006A71] hover:bg-[#006A71] focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-800 dark:hover:bg-gray-800 dark:hover:shadow-2xl"
         >
           Search
         </button>
